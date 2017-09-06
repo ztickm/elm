67049093bde406ab27c9c5d25f7b214af3438c05
Salim Mahboubi <ztickm@gmail.com>
2017-09-06 11:13:18 +0100

    Added some comments on exercise 3

--- 02 more on functions/Exercise3.elm
+++ 02 more on functions/Exercise3.elm
@@ -6,7 +6,14 @@ import String
 
 wordCount : String -> Int
 wordCount =
+    --First Try    --String.words sentence >> List.length
     String.words >> List.length
+
+
+
+-- I didn't cheat I swear!
+--I checked this
+-- https://csmith111.gitbooks.io/functional-reactive-programming-with-elm/content/section2/FunctionComposition.html
 
 
 main =
